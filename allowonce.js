6f0669c709523171b9470b0aefa7437e00bb9711
sumit021995 <kaku.saurav@gmail.com>
2024-01-18 13:30:50 +0530

    file updated

--- allowonce.js
+++ allowonce.js
@@ -11,3 +11,24 @@ Every subsequent time it is called, it should return undefined.
     
 // } while (condition);
 
+var once = function(fn) {
+    let calls=false;
+    let result 
+    return function(...args){
+       if (!calls) {
+      result = fn.apply(this, args);
+      calls = true;
+      return result;
+    } else {
+      return undefined;
+    }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
