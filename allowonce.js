1881d53fd7aa20d9fc97332bd597e46915457402
sumit021995 <kaku.saurav@gmail.com>
2024-01-18 20:39:03 +0530

    file updated

--- allowonce.js
+++ allowonce.js
@@ -32,3 +32,16 @@ var once = function(fn) {
  * onceFn(1,2,3); // 6
  * onceFn(2,3,6); // returns undefined without calling fn
  */
+
+var once = function(fn) {
+    let called = false;
+
+	return function(...args){
+        if(called)
+            return undefined;
+        
+        called = true;
+        return fn(...args);
+    }
+};
+ 
